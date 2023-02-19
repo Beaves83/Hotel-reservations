7fbd44ae93df47e4fea7438ed0f2a53379a0a314
Bibi <beaves83@gmail.com>
2023-02-19 23:41:12 +0100

    Refactor: apply best practices in tailwinds

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,22 @@ module.exports = {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
-  theme: {
-    colors: {
-      'white': '#FFFFFF',
-      'black': '#000000',
-      'primary': '#0162B3',
-      'gray': {
-        DEFAULT: '#4A4A4A',
-        light: '#D5D5D5',
+  theme: {  
+    extend: {
+      colors: {
+        white: '#FFFFFF',
+        black: '#000000',
+        primary: '#0162B3',
+        gray: {
+          DEFAULT: '#4A4A4A',
+          light: '#D5D5D5',
+        },
+        },
+      fontFamily: {
+        display: ['Roboto Slab', 'serif'],
+      },
+
       },
     },
-    fontFamily: {
-      display: ['Roboto Slab', 'serif'],
-    },
-    extend: {},
-  },
   plugins: [],
 }
