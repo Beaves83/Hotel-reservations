6b862d11cf01e2e9a35618c9d6704a2e2709d05f
Bibi <beaves83@gmail.com>
2023-02-21 21:50:54 +0100

    feat: add room creation

--- src/components/Rooms.vue
+++ src/components/Rooms.vue
@@ -1,23 +1,19 @@
 <script setup lang="ts">
+import { defineEmits } from 'vue';
 
 import img1 from '@/assets/img/room_1.png';
 import img2 from '@/assets/img/room_2.png';
 import img3 from '@/assets/img/room_3.png';
-import { Room } from '@/src/interfaces.ts'
+import { Room } from '@/interfaces';
 
-
-
-const props = defineProps<{ 
-    selectRoom: {
-      type: Function, // type: () => void; esto es lo recomendado
-      required: true,
-    }, 
-}>();
+//Emits
+const emit = defineEmits(['selectRoom']);
 
 const myRooms: Room[] = [
   {
     name: 'Mini Dreamy Room',
-    description: 'Generous and comfortable these modern furnished rooms offer two queen-size beds and are on the first floor.',
+    description:
+      'Generous and comfortable these modern furnished rooms offer two queen-size beds and are on the first floor.',
     size: '20m2',
     beds: 2,
     people: 2,
@@ -26,7 +22,8 @@ const myRooms: Room[] = [
   },
   {
     name: 'Seet Bungalow',
-    description: 'The perfect blend of comfort and culture, our superior room with a central graden view has the stunning, and comes with a jacuzzi.',
+    description:
+      'The perfect blend of comfort and culture, our superior room with a central graden view has the stunning, and comes with a jacuzzi.',
     size: '50m2',
     beds: 2,
     people: 4,
@@ -35,7 +32,8 @@ const myRooms: Room[] = [
   },
   {
     name: 'Los Cocos Suite',
-    description: 'If you want a little extra form your stay, you might like our superior rooms. A ocean view room has a private beach and a helicopter :-).',
+    description:
+      'If you want a little extra form your stay, you might like our superior rooms. A ocean view room has a private beach and a helicopter :-).',
     size: '125m2',
     beds: 3,
     people: 4,
@@ -44,50 +42,50 @@ const myRooms: Room[] = [
   },
 ];
 
-const selectRoom = (room: Room) => {
-  props.selectRoom(room);
+const selectRoom = (selectedRoom: Room) => {
+  emit('selectRoom', selectedRoom);
 };
 </script>
 <template>
   <div class="space-y-4">
     <!-- TODO: Aplicar la deconstruccion {name, descr...} in myRooms si si en selectRoom al final solo necesitamos el id -->
     <div
-      v-for="room in myRooms" 
-      :key="room.name"
+      v-for="myRoom in myRooms"
+      :key="myRoom.name"
       class="room"
-      @click="selectRoom(room)"
+      @click="selectRoom(myRoom)"
     >
       <div class="flex space-x-4">
         <div class="w-1/3">
-          <img 
-            :src="room.image" 
-            alt="Mini Dreamy Room" 
+          <img
+            :src="myRoom.image"
+            alt="Mini Dreamy Room"
             class="object-cover w-full h-full"
           />
         </div>
         <div class="flex flex-col w-2/3 text-sm">
           <p class="mb-2 text-base font-display font-bold">
-            {{ room.name || '' }}
+            {{ myRoom.name || '' }}
           </p>
-          <p class="flex-1">{{ room.description }}</p>
-          <p class="mb-6">Size: {{ room.size }}</p>
+          <p class="flex-1">{{ myRoom.description }}</p>
+          <p class="mb-6">Size: {{ myRoom.size }}</p>
           <div class="flex items-baseline space-x-16">
             <div>
-              <img src='@/assets/img/double-bed.svg' class="w-8 mb-1" />
-              <p>Beds: {{ room.beds }}</p>
+              <img src="@/assets/img/double-bed.svg" class="w-8 mb-1" />
+              <p>Beds: {{ myRoom.beds }}</p>
             </div>
-            <div class="flex-1">People: {{ room.people }}</div>
-          <div class="text-xl font-bold">
-            {{ room.amount }}
+            <div class="flex-1">People: {{ myRoom.people }}</div>
+            <div class="text-xl font-bold">
+              {{ myRoom.amount }}
+            </div>
           </div>
-        </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 <style scoped>
-.room{
+.room {
   @apply p-4 transition-all duration-300 border cursor-pointer border-gray-light hover:bg-gray-light hover:bg-opacity-20 hover:shadow-sm;
 }
 </style>
--- src/components/RoomsSelector.vue
+++ src/components/RoomsSelector.vue
@@ -3,28 +3,25 @@ import { ref } from 'vue';
 
 import Rooms from '@/components/Rooms.vue';
 import Summary from '@/components/Summary.vue';
+import { Room } from '@/interfaces';
 
 const summary = ref(null); //todo: no es un string?
+const room: Room = ref({
+  name: '',
+});
 
-const selectRoom = (room: any) => {
-  if (summary.value) {
-    summary.value.updateHotelData(room);
-  }
+const selectRoom = (selectedRoom: Room) => {
+  room.value = selectedRoom;
 };
-
-
 </script>
 <template>
- <div class="inline-block px-8 mb-16">
+  <div class="inline-block px-8 mb-16">
     <div class="float-left w-2/3">
-      <Rooms :select-room="selectRoom"
-      />
+      <Rooms @selectRoom="selectRoom" />
     </div>
     <div class="float-right w-1/3 pl-4">
-      <Summary ref="summary" />
+      <Summary ref="summary" :room="room" />
     </div>
   </div>
 </template>
-<style scoped>
-
-</style>
+<style scoped></style>
